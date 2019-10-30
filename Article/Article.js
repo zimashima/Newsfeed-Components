bf0af16dc8f454039bf473a4e2f8a7bf95fee792
Mashima <52222646+zimashima@users.noreply.github.com>
2019-10-30 15:19:40 -0400

    created a function with definition and class. not complete yet

--- Article/Article.js
+++ Article/Article.js
@@ -112,3 +112,27 @@ const data = [
   Step 5: Add a new article to the array. Make sure it is in the same format as the others. Refresh the page to see the new article.
 
 */
+
+function newsArticle(title, date, firstParagraph, secondParagraph, thirdParagraph){
+  
+  // definition 
+  const article = document.createElement('div')
+  const newsTitle = document.createElement('h2')
+  const newsDate = document.createElement('p')
+  const paragraphOne = document.createElement('p')
+  const paragraphTwo = document.createElement('p')
+  const paragraphThree = document.createElement('p')
+  const spanButton = document.createElement('span')
+
+  // structure
+
+  //class
+  article.classList.add('article')
+  newsDate.classList.add('date')
+  spanButton.classList.add('expandButton')
+  
+
+
+
+  return article
+}
